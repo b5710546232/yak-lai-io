0d041f3f97306cd49b516c7b12aa23dd9ed5d166
b5710546232 <nattapat.su@ku.th>
2017-05-11 18:18:45 +0700

    fix server

--- server/server.js
+++ server/server.js
@@ -10,11 +10,11 @@ var cors = require('cors')
 // });
 app.use(cors())
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', "*")
-  res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE')
-  res.header('Access-Control-Allow-Headers', 'Content-Type')
-  next()
-})
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE')
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+});
 
 var color = ['#999999', '#CCCCCC', '#00FF00', '#0000FF', '#FF0000', '#FFFF00'];
 var users = [];
